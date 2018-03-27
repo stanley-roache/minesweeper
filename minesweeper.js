016cb8e3d8c1b2b775b8ac9914e53e89c198b906
Stanley Roache <stan.roache@gmail.com>
2018-03-27 14:02:10 +1300

    can restart game when finished

--- minesweeper.js
+++ minesweeper.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-let cells = [];
-const boardEdge = 5;
-for (var i = 0; i < Math.pow(boardEdge,2); i++) {
-  cells.push(
-    {
-      row: Math.floor(i/boardEdge),
-      col: i%boardEdge,
-      isMine: (Math.random() > 0.9),
-      hidden: true,
-    }
-  );
+var board = {
+  boardEdge: 6,
+  cells: [],
+};
+
+for (var i = 0; i < Math.pow(board.boardEdge,2); i++) {
+  board.cells.push({
+    row: Math.floor(i/board.boardEdge),
+    col: i%board.boardEdge,
+    // hidden: false,
+    // isMine: false,
+    // isMarked: false,
+    // isProcessed: false,
+    // surroundingMines: 0,
+  });
 }
 
-var board = {
-  boardEdge: boardEdge,
-  cells: cells.slice()
-}; 
+resetBoard();
 
 function startGame () {
-  for (var i = 0; i < board.cells.length; i++) {
-    cells[i].surroundingMines = countSurroundingMines(cells[i]);
-  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 
   document.addEventListener('click', checkForWin);
   document.addEventListener('contextmenu', checkForWin);
+
+  
 }
 
 // Define this function to look for a win condition:
@@ -36,14 +36,19 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
   for (var i = 0; i < board.cells.length; i++) {
-    let currCell = cells[i]
+    let currCell = board.cells[i]
     if (currCell.isMine && (!currCell.isMarked)) return;
     else if ((!currCell.isMine) && currCell.hidden) return;
+    else if ((currCell.isMine) && (!currCell.hidden)) {
+      
+    }
   }
+
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  lib.displayMessage('You win!')
+  lib.displayMessage('You win! Press any key to play again')
+  document.addEventListener('keypress', restartGame);
 }
 
 // Define this function to count the number of mines around the cell
@@ -60,5 +65,24 @@ function countSurroundingMines (cell) {
       return cell.isMine;
     })
     .length;
+}
+
+function resetBoard() {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].isMine = (Math.random() > 0.8);
+    board.cells[i].isMarked = false;
+    board.cells[i].hidden = true;
+    board.cells[i].isProcessed = false;
+  }
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+}
+
+function restartGame(e) {
+  document.getElementsByClassName('board')[0].innerHTML="";
+  resetBoard();
+  document.removeEventListener('keypress', restartGame);
+  startGame();
 }
 
